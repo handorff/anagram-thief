c4f61afb3fed407dc76a20a8e62351c917806261
Paul Handorff <handorff.paul@gmail.com>
2026-02-09 08:45:33 -0500

    Scope root styles and gate Agent

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles.css";
+import { Agentation } from "agentation";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
+    {import.meta.env.DEV ? <Agentation /> : null}
     <App />
   </React.StrictMode>
 );
